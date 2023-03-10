723df59e8a90e077c7b355d42d4511ef2f78df3f
F. Camuncoli <f.camuncoli@gmail.com>
2023-03-10 18:55:41 -0500

    imported background img

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary.js";
 import Footer from "./Footer";
+
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo img-fluid" alt="logo" /> */}
-
+       
           <h1>Dictionary</h1>
         </header>
 
