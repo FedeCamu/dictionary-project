d5b67abf0852387b49264c52919658c4a1fc36f6
F. Camuncoli <f.camuncoli@gmail.com>
2023-03-10 22:29:13 -0500

    added photos API and component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
+
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null); 
 
   function handleResponse(response) {
     setResults(response.data[0]);
@@ -14,9 +17,19 @@ export default function Dictionary(props) {
 
   ///API documentation: https://dictionaryapi.dev////
 
+  function handlePexelsResponse(response){
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey = "6xvFjmNlNDUeWBjAo4GFvGd1MvgCyFVfUqNDm3jEvJRQU3bbEkjf0h4q";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+
+    let headers = {Authorization: `${pexelsApiKey}`};
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -47,6 +60,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -7,14 +7,14 @@ export default function Phonetics(props) {
   return (
     <div className="Phonetics">
       <div className="row align-items-center mb-3">
-        <div className="col">
+        <div className="col-4">
           <ReactAudioPlayer
             src={props.phonetics.audio}
             controls
             className="player"
           />
         </div>
-        <div className="col-9 text">{props.phonetics.text}</div>
+        <div className="col-4 text text-start">{props.phonetics.text}</div>
       </div>
     </div>
   );
